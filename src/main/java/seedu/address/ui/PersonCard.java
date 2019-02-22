72df99edbbc136e71da4e0f25e4ba26536ea961b
erjillsison <erjillsison@gmail.com>
2019-02-22 08:10:25 +0800

    Empty out the person card views

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -42,12 +42,12 @@ public class PersonCard extends UiPart<Region> {
     public PersonCard(Person person, int displayedIndex) {
         super(FXML);
         this.person = person;
-        id.setText(displayedIndex + ". ");
-        name.setText(person.getName().fullName);
-        phone.setText(person.getPhone().value);
-        address.setText(person.getAddress().value);
-        email.setText(person.getEmail().value);
-        person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        //id.setText(displayedIndex + ". ");
+        //name.setText(person.getName().fullName);
+        //phone.setText(person.getPhone().value);
+        //address.setText(person.getAddress().value);
+        //email.setText(person.getEmail().value);
+        //person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 
     @Override

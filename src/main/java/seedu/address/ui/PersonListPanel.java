22529c47c90f4c543093688f9fddbce58b052903
erjillsison <erjillsison@gmail.com>
2019-03-01 11:14:40 +0800

    Change PersonListPanel HBox to Vbox

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -11,6 +11,7 @@ import javafx.scene.control.ListCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -28,7 +29,7 @@ public class PersonListPanel extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
     @FXML
-    private HBox row;
+    private VBox rows;
 
     public PersonListPanel(ObservableList<Cell> cellList, ObservableValue<Cell> selectedPerson,
                            Consumer<Cell> onSelectedPersonChange) {
@@ -37,7 +38,7 @@ public class PersonListPanel extends UiPart<Region> {
 
 
         cellList.addListener((ListChangeListener<Cell>) p -> {
-            row.getChildren().clear();
+            rows.getChildren().clear();
             repopulateGrid(cellList);
         });
     }
@@ -48,34 +49,40 @@ public class PersonListPanel extends UiPart<Region> {
     void repopulateGrid(ObservableList<Cell> personList) {
 
         for (int i = 0; i < personList.size(); i++) {
-            Rectangle cell = new Rectangle(30, 30);
-            cell.setStroke(Color.BLACK);
-            cell.setFill(Color.WHITE);
-            Text text = new Text("");
+            HBox row = new HBox();
 
-            switch (personList.get(i).getStatus()) {
-            case HIDDEN:
-                text = new Text("H");
-                break;
-            case SHIP:
-                text = new Text("B");
-                break;
-            case HIT:
-                text = new Text("X");
-                break;
-            case DESTROYED:
-                break;
-            case EMPTY:
-                text = new Text("M");
-                break;
-            default:
-                text = new Text("");
+            for (int j = 0; j < personList.size(); j++) {
+                Rectangle cell = new Rectangle(30, 30);
+                cell.setStroke(Color.BLACK);
+                cell.setFill(Color.WHITE);
+                Text text = new Text("");
 
+                switch (personList.get(i).getStatus()) {
+                case HIDDEN:
+                    text = new Text("H");
+                    break;
+                case SHIP:
+                    text = new Text("B");
+                    break;
+                case HIT:
+                    text = new Text("X");
+                    break;
+                case DESTROYED:
+                    break;
+                case EMPTY:
+                    text = new Text("M");
+                    break;
+                default:
+                    text = new Text("");
+
+                }
+
+                StackPane sp = new StackPane();
+                sp.getChildren().addAll(cell, text);
+                row.getChildren().add(sp);
             }
 
-            StackPane sp = new StackPane();
-            sp.getChildren().addAll(cell, text);
-            row.getChildren().add(sp);
+            rows.getChildren().add(row);
         }
     }
 
@@ -85,14 +92,22 @@ public class PersonListPanel extends UiPart<Region> {
      */
     void populateGrid(ObservableList<Cell> personList) {
         for (int i = 0; i < personList.size(); i++) {
-            Rectangle cell = new Rectangle(30, 30);
-            cell.setStroke(Color.BLACK);
-            cell.setFill(Color.WHITE);
-            Text text = new Text("");
-            StackPane sp = new StackPane();
-            sp.getChildren().addAll(cell, text);
-            row.getChildren().add(sp);
+            HBox row = new HBox();
+
+            for (int j = 0; j < personList.size(); j++) {
+                Rectangle cell = new Rectangle(30, 30);
+                cell.setStroke(Color.BLACK);
+                cell.setFill(Color.WHITE);
+                Text text = new Text("");
+
+                StackPane sp = new StackPane();
+                sp.getChildren().addAll(cell, text);
+                row.getChildren().add(sp);
+            }
+
+            rows.getChildren().add(row);
         }
+
     }
 
     /**
